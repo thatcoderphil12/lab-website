d47a6333720d961b67f234a32d6891ed54b52df4
thatcoderphil12 <phil.f.murphy@icloud.com>
2026-03-14 20:14:42 -0400

    Fixed bug

--- commands.sh
+++ commands.sh
@@ -4,17 +4,17 @@
 sudo systemctl start nginx
 
 # Activate environment
-source $HOME/lab-website/venv/bin/activate
+source ~/lab-website/venv/bin/activate
 
 # Run paper 1
-cd $HOME/lab-website/app/paper-1/ || exit
+cd ~/lab-website/app/paper-1/ || exit
 nohup streamlit run app.py \
   --server.port 8080 \
   --server.baseUrlPath paper1 \
   > paper1.log 2>&1 &
 
 # Run paper 2
-cd $HOME/lab-website/app/paper-2/ || exit
+cd ~/lab-website/app/paper-2/ || exit
 nohup streamlit run app.py \
   --server.port 8081 \
   --server.baseUrlPath paper2 \
