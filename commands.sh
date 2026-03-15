a7bc3fab558213b8d02dc2129c74c14a176cd3a9
thatcoderphil12 <phil.f.murphy@icloud.com>
2026-03-14 20:13:27 -0400

    Added commands.sh

---
+++ commands.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Start nginx
+sudo systemctl start nginx
+
+# Activate environment
+source $HOME/lab-website/venv/bin/activate
+
+# Run paper 1
+cd $HOME/lab-website/app/paper-1/ || exit
+nohup streamlit run app.py \
+  --server.port 8080 \
+  --server.baseUrlPath paper1 \
+  > paper1.log 2>&1 &
+
+# Run paper 2
+cd $HOME/lab-website/app/paper-2/ || exit
+nohup streamlit run app.py \
+  --server.port 8081 \
+  --server.baseUrlPath paper2 \
+  > paper2.log 2>&1 &
+
+# Restart nginx
+sudo systemctl restart nginx
